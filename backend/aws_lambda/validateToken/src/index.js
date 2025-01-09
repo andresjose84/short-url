ffc6f1346cf9aea97f42fe397e92d903a5adcdb3
Andres Jose Sanchez <andresjose84@gmail.com>
2025-01-09 11:29:05 -0500

    validateToken

--- backend/aws_lambda/validateToken/src/index.js
+++ backend/aws_lambda/validateToken/src/index.js
@@ -1,12 +1,32 @@
+const dotenv = require( 'dotenv' );
 
-const app = async event => {
-    return {
-        statusCode: 200,
-        body: "",
-        headers: {
-            "Content-Type": "application/json"
+const validateToken = require( './controllers/token.controller' );
+
+const app = event => {
+
+    dotenv.config();
+
+    let authHeader = '';
+
+    if ( !event.headers.authorization ) {
+        if ( event.headers.Authorization ) {
+            authHeader = event.headers.Authorization;
+        } else {
+            return {
+                statusCode: 401,
+                body: JSON.stringify( {
+                    ok: false,
+                    message: 'No token provided'
+                } )
+            }
         }
+    } else {
+        authHeader = event.headers.authorization;
     }
+
+    const token = authHeader.split( ' ' )[ 1 ];
+
+    return validateToken( token );
 }
 
 module.exports = app;
